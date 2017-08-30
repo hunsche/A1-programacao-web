00f64dbd9403b6f823e453f5f9038570f6e2ceb7
Matheus <matheus.arendt.hunsche@gmail.com>
2017-08-29 23:52:45 -0300

    project finalized

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # A1-programacao-web
+
+deploy realizado em:
+    - https://a1-programacao-web.herokuapp.com/
--- js/cartola.js
+++ js/cartola.js
@@ -1,22 +1,24 @@
 function ordenar() {
     var $table = $('#table');
+    $table.bootstrapTable('removeAll');
+    var mydata = [];
     var obj1 = {
         "pos": null,
         "nome": document.getElementById("nome1").value,
         "pont": document.getElementById("pontuacao1").value
-    }
+    };
     var obj2 = {
         "pos": null,
         "nome": document.getElementById("nome2").value,
         "pont": document.getElementById("pontuacao2").value
-    }
+    };
     var obj3 = {
         "pos": null,
         "nome": document.getElementById("nome3").value,
         "pont": document.getElementById("pontuacao3").value
-    }
+    };
     var mydata = [obj1, obj2, obj3];
-
+    console.log(mydata);
     function compare(a, b) {
         if (a.pont > b.pont)
             return -1;
@@ -26,9 +28,17 @@ function ordenar() {
     }
     mydata.sort(compare);
 
+    mydata[0].pos = '<img onclick="mudar4()" class="card-img-top" width="20" height="20" src="../img/primeiro.jpg">';
+    mydata[1].pos = '<img onclick="mudar4()" class="card-img-top" width="20" height="20" src="../img/segundo.png">';
+    mydata[2].pos = '<img onclick="mudar4()" class="card-img-top" width="20" height="20" src="../img/terceiro.jpg">';
+
     $(function () {
         $('#table').bootstrapTable({
             data: mydata
         });
     });
+}
+
+function reload() {
+    location.reload();
 }
